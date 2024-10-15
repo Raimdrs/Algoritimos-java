c4b63905a72612e088347a9590b8de0490d5e7d0
Raimdrs <raiimdrs@gmail.com>
2024-10-15 19:58:56 -0300

    Exemplo_mario

--- L3/src/Pessoa.java
+++ L3/src/Pessoa.java
@@ -1,15 +1,18 @@
 public class Pessoa {
     int idade = 28;
 
-    public void fazerAniversario(){
-        this.idade++;
+    public int getIdade(){
+        return this.idade;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
     }
 
     public static void main(String[] args) {
         Pessoa gabriel = new Pessoa();
-        
-        System.out.println(gabriel.idade);
-        gabriel.fazerAniversario();
-        System.out.println(gabriel.idade);
+
+        System.out.println(gabriel.getIdade());
+        gabriel.setIdade(gabriel.getIdade() + 1);
+        System.out.println(gabriel.getIdade());
     }
 }
